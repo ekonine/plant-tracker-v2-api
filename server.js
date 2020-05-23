f3eabf4eff5edc917720812ccf9bc7865aa22645
ekonine <xeon.slyde@gmail.com>
2020-05-23 02:52:21 -0500

    add server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+
+require('dotenv').config();
+
+const app = express();
+const port = 3001;
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).json('Wow it works');
+})
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`)
+})
